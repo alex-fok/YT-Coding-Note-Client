911dc3895106431fb63d92854f302aef27c95d21
Alex Fok <foktmalexander@gmail.com>
2022-11-11 05:28:06 -0800

    Change theme

--- src/app/editor/services/editor-view.service.ts
+++ src/app/editor/services/editor-view.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
-import { EditorView, EditorViewConfig, lineNumbers, highlightActiveLineGutter } from '@codemirror/view';
+import { EditorView, EditorViewConfig, lineNumbers, highlightActiveLine } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
+
+const darkTheme = EditorView.theme({
+  "&": {
+    color: "white",
+    backgroundColor: "#212121",
+    height: '100%'
+  },
+  "&.cm-focused": {
+    outline: 'none !important'
+  },
+  "&.cm-focused .cm-selectionBackground, ::selection": {
+    backgroundColor: "#6c6c6c"
+  },
+  ".cm-gutters": {
+    backgroundColor: "#212121",
+    color: "#d0d0d0",
+    padding: "0em 1em"
+  },
+  ".cm-activeLine": {
+    backgroundColor: "#3c3c3c"
+  }
+}, {dark: true})
 
 const config: EditorViewConfig = {
   extensions: [
     lineNumbers(),
-    highlightActiveLineGutter()
+    highlightActiveLine(),
+    darkTheme
   ]
 }
 
@@ -15,9 +38,7 @@ const config: EditorViewConfig = {
 export class EditorViewService {
   view = new EditorView({...config});
   
-  constructor() {
-    this.view.dom.style.height = '100%';
-  }
+  constructor() { }
 
   getView(content?: string): HTMLElement {
     if (content)
