86a131aa4c600d615b03bec9f205d3b9a1297fe6
Alex Fok <foktmalexander@gmail.com>
2022-11-25 00:44:37 -0800

    Use file selection and content service for getting file in view

--- src/app/editor/code-editor/code-editor.component.ts
+++ src/app/editor/code-editor/code-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ContentService } from '../services/content.service';
-import { Subject } from 'rxjs';
 import { EditorViewService } from '../services/editor-view.service';
+import { FileSelectionService } from 'src/app/services/file-selection.service';
 
 @Component({
   selector: 'app-code-editor',
@@ -10,46 +10,21 @@ import { EditorViewService } from '../services/editor-view.service';
 })
 export class CodeEditorComponent implements OnInit, AfterViewInit {
   @ViewChild('editorContainer', {static: false}) container!: ElementRef;
-  
-  @Input()
-  get timeSegment(): number { return this._timeSegment; }
-  set timeSegment(t: number) {
-    this._timeSegment = t;
-    this.viewUpdater$.next({
-      fileName: this._fileName,
-      timeSegment: this._timeSegment
-    });
-  } 
-  @Input()
-  get fileName(): string { return this._fileName };
-  set fileName(fname: string) {
-    this._fileName = fname;
-    this.viewUpdater$.next({
-      fileName: fname,
-      timeSegment: this._timeSegment
-    });
-  };
   private view: HTMLElement;
-  private viewUpdater$ = new Subject<{fileName: string, timeSegment: number}>();
-  private _timeSegment = 0;
-  private _fileName = '';
   
   constructor(
     private contentService: ContentService,
     private editorViewService: EditorViewService,
+    private fileSelectionService: FileSelectionService,
     private renderer: Renderer2,
   ) {
     this.view = this.editorViewService.getView();
   }
   ngOnInit(): void {
-    this.viewUpdater$.subscribe(async({fileName, timeSegment}) => {
-      const content = await this.contentService.getContent(fileName, timeSegment);
-      this.view = this.editorViewService.getView(content);
-    });
-    this.viewUpdater$.next({
-      fileName: this._fileName,
-      timeSegment: this._timeSegment
-    });
+    this.fileSelectionService.getFileViewedSubject().subscribe(async(selected) => {
+      const content = await this.contentService.getContent(selected.itemId);
+      this.view = this.editorViewService.getView(content)
+    })
   }
   ngAfterViewInit(): void {
     this.renderer.appendChild(this.container.nativeElement, this.view);
