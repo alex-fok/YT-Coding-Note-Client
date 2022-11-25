8de7f3b2dfe2af89bca344e25f68209bcdbc1c5b
Alex Fok <foktmalexander@gmail.com>
2022-11-25 00:22:39 -0800

    Add file selection service

--- src/app/services/file-selection.service.ts
+++ src/app/services/file-selection.service.ts
@@ -1,17 +1,60 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { FileStructService } from './file-struct.service';
+
+import type { FileDB } from 'types/videoInfo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileSelectionService {
-  fileSelected$ = new Subject<string>();
-  constructor() { }   
+  fileViewedId = '';
+  fileViewed$ = new Subject<FileDB>();
+  dirSelected$ = new Subject<string>();
+  filesOpened: FileDB[] = []
+  filesOpened$ = new Subject<FileDB[]>();
+  
+  constructor(private fileStructService: FileStructService) {
+    this.fileStructService.getFileStructSubject().subscribe(fileStruct => {
+      this.filesOpened = this.filesOpened.map(({id}) => fileStruct[id] as FileDB);
+      this.filesOpened$.next(this.filesOpened);
 
-  getFileSelectedSubject() {
-    return this.fileSelected$;
+      if (this.fileViewedId) {
+        const file = fileStruct[this.fileViewedId] as FileDB;
+        this.fileViewedId = file.id;
+        this.fileViewed$.next(file);
+      }
+    })
   }
-  updateFileSelected(id: string) {
-    this.fileSelected$.next(id)
+  getDirSelectedSubject() {
+    return this.dirSelected$;
+  }
+  getFileViewedSubject() {
+    return this.fileViewed$;
+  }
+  updateDirSelected(id: string) {
+    const obj = this.getDBObj(id)
+    this.dirSelected$.next(id)
+    
+    if (obj.type === 'file') {
+      this.fileViewedId = obj.id;
+      this.fileViewed$.next(obj);
+    }
+  }
+  addFile(id: string) {
+    const obj = this.getDBObj(id);
+    if (obj.type !== 'file') return;
+    // typeof obj -> FileDB
+    this.filesOpened.push(obj);
+    this.filesOpened$.next(this.filesOpened);
+    
+    this.fileViewedId = obj.id;
+    this.fileViewed$.next(obj);
+  }
+  removeFile(id: string) {
+    this.filesOpened = this.filesOpened.filter(file => file.id !== id);
+  }
+  private getDBObj(id: string) {
+    return this.fileStructService.getFileSysDBObj(id);
   }
 }
