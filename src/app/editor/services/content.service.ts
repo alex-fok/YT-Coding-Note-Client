e5b78be5ab59a509d32802c24cfe7ea3e5096302
Alex Fok <foktmalexander@gmail.com>
2022-11-25 00:42:09 -0800

    Refactor Content Serivce, which now fetches and stores content

--- src/app/editor/services/content.service.ts
+++ src/app/editor/services/content.service.ts
@@ -1,38 +1,23 @@
 import { Injectable } from '@angular/core';
-import { FileNameService } from 'src/app/services/file-name.service';
-
-type fileDB = {id: number, file: string, content: string}
+import type { FileContent } from 'types/fileContent';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-  private fileNames!: Record<string, Record<string, string>>;
-  private videoId = '';
+  // itemId : content
+  private fileStore: Record<string, string> = {};
 
-  constructor(private fileNameService: FileNameService) { }
-  async getContent(fileName: string, timeSegment: number): Promise<string> {
-    if (!this.videoId) return '';
-    if (!this.fileNames) await this.getFileNames();
-    return this.getFile(this.fileNames[timeSegment][fileName]) || '';
+  constructor() { }
+  async getContent(itemId: string): Promise<string> {
+    const content = this.fileStore[itemId] || await this.getFile(itemId);
+    return content || '';
   }
-  updateVideoId(id: string) {
-    this.videoId = id
-  }
-  private async getFileNames() {
-    if (!this.videoId) return;
-    const files = await this.fileNameService.getFileNames(this.videoId);
-    const tmp:Record<string, Record<string, string>> = {};
+  private async getFile(itemId: string) {
+    const files: FileContent[] =
+      await fetch(`http://localhost:3000/files?id=${itemId}`).then(result => result.json());
 
-    files.forEach(f => {
-      if (!tmp[f.tSeg]) tmp[f.tSeg] = {};
-      tmp[f.tSeg][f.name] = f.file;
-    });
-    this.fileNames = tmp;
-  }
-  private async getFile(fileName: string) {
-    const files: fileDB[] =
-      await fetch(`http://localhost:3000/files?file=${fileName}`).then(result => result.json());
+    this.fileStore[itemId] = files[0]?.content;
     return files[0]?.content;
   }
 }
