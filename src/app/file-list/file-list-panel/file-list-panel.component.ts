c8ef6e720058b73a9b02b1bce3b2b7f4c08e2d1c
Alex Fok <foktmalexander@gmail.com>
2022-12-03 05:49:46 -0800

    Scale file list by window width

--- src/app/file-list/file-list-panel/file-list-panel.component.ts
+++ src/app/file-list/file-list-panel/file-list-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FileDirectoryService } from 'src/app/services/file-directory.service';
 
 type FileSysObj = {id: string, type: 'file' | 'folder' | 'root', name: string}
@@ -10,11 +10,17 @@ type FileSysObj = {id: string, type: 'file' | 'folder' | 'root', name: string}
 })
 export class FileListPanelComponent implements OnInit {
   rootFiles: FileSysObj[] = [];
+  minWidth: number = 0;
   constructor(private fileDirService: FileDirectoryService) { }
 
  ngOnInit(): void {
     this.fileDirService.getDirTreeSubject().subscribe(tree => {
       this.rootFiles = tree['/'] || []
     })
+    this.getPanelMinWidth()
+  }
+  @HostListener('window:resize')
+  getPanelMinWidth() {
+    this.minWidth = window.innerWidth / 10
   }
 }
